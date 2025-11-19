8e6b5d60fbba50e849318e348163fcc194b95594
riaxash16 <ria.xash16@gmail.com>
2025-11-19 22:20:24 +0400

    Simplify even/odd check in main function
    Refactored even/odd check into main function.

--- Exercise 10 - Is it even/Exercise 10.py
+++ Exercise 10 - Is it even/Exercise 10.py
@@ -1,14 +1,8 @@
-def check_even_odd(number):
-  if number % 2 == 0:
-    return f"{number} is even."
-  else:
-    return f"{number} is odd."
-
 def main():
-  """
-  Main function to get user input, call the even/odd checker, and print the result.
-  """
-
-
+  num = int(input("Enter a number: "))
+    if num % 2 == 0: #if remainder is 0
+      print(f"{num} is even")
+    else
+      print(f"{num} is odd")
 if __name__ == "__main__":
-  main()
+    main()
